747d21e63b6f30804dce26641287af29b0c6be64
shrinivasmandhare7 <shrinivasmandhare7@gmail.com>
2023-11-14 20:47:09 +0530

    Added Comment and Formatted the code

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -1,18 +1,19 @@
-const accountId=1234;
-let accountEmail="Shrinivasmandhare2000@gmail.com";
-var accountPassword="Test@123";
+//Javascript is Dynamically Typed Language
+const accountId = 1234;
+let accountEmail = "Shrinivasmandhare2000@gmail.com";
+var accountPassword = "Test@123";
 /*
 Do Not Use var .Because of the issue of Block scope and Functional Scope.
 */
 
-accountCity="Pune";//This is aallowed but its not a good practice.
+accountCity = "Pune";//This is aallowed but its not a good practice.
 
 let accountState;
 
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
 
 // accountId=3212; Not Allowed
-accountEmail="shrinivasmandhare7@gmail.com";
-accountPassword="Pass@123";
-accountCity="Bangluru";
-console.table([accountId,accountEmail,accountPassword,accountCity])
+accountEmail = "shrinivasmandhare7@gmail.com";
+accountPassword = "Pass@123";
+accountCity = "Bangluru";
+console.table([accountId, accountEmail, accountPassword, accountCity])
