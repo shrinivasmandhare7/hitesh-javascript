caed88f4ee577bef08d944c44766bb6861d73cf1
shrinivasmandhare7 <shrinivasmandhare7@gmail.com>
2023-11-14 13:48:56 +0530

    Added Operations.js

---
+++ 01_Basics/04_Operations.js
@@ -0,0 +1,13 @@
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 3);
+console.log(2 ** 3);
+console.log(2 / 2);
+console.log(2 % 2);
+
+console.log("1" + "2");//12
+console.log("1" + 2);//12
+console.log(1 + "2");//12
+console.log("1" + 2 + 2);//122 if the string is first operand then all operands will be treated as strings
+console.log(1 + 2 + "2");//32 if the string is last operand then all other operation will be performed first
+
