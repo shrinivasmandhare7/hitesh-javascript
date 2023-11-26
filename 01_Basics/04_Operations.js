f4f251045fefcb126ae58fe6a47995e94d4ed9ec
shrinivasmandhare7 <74829146+shrinivasmandhare7@users.noreply.github.com>
2023-11-26 06:46:40 +0000

    Added Nullish coalescing operator in 04_Operations

--- 01_Basics/04_Operations.js
+++ 01_Basics/04_Operations.js
@@ -5,9 +5,14 @@ console.log(2 ** 3);
 console.log(2 / 2);
 console.log(2 % 2);
 
-console.log("1" + "2");//12
-console.log("1" + 2);//12
-console.log(1 + "2");//12
-console.log("1" + 2 + 2);//122 if the string is first operand then all operands will be treated as strings
-console.log(1 + 2 + "2");//32 if the string is last operand then all other operation will be performed first
+console.log("1" + "2"); //12
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122 if the string is first operand then all operands will be treated as strings
+console.log(1 + 2 + "2"); //32 if the string is last operand then all other operation will be performed first
 
+// Nullish coalescing operator
+console.log(5 ?? 10);
+console.log(null ?? 10);
+console.log(undefined ?? 10);
+console.log(undefined ?? 10 ?? 20); //will return first non null/undefined value
